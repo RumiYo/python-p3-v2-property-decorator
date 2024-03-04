e567447ef4494e1c8566c8fb5e527dcbda82d843
RumiYo <yokoyamarumi@gmail.com>
2024-03-03 20:35:37 -0500

    Complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -15,25 +15,52 @@ class Dog:
         self.name = name
         self.breed = breed
 
-    def get_name(self):
-        return self._name
+    # def get_name(self):
+    #     return self._name
 
-    def set_name(self, name):
-        if isinstance(name, str) and 1 <= len(name) <= 25:
-            self._name = name.title()
+    @property
+    def name(self):
+        """The name property"""
+        return self._name 
+
+    # def set_name(self, name):
+    #     if isinstance(name, str) and 1 <= len(name) <= 25:
+    #         self._name = name.title()
+    #     else:
+    #         raise ValueError(
+    #             "Name must be string between 1 and 25 characters.")
+
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 1 and 25 characters in length"""
+        if isinstance(name, str) and 1<= len(name) <= 25:
+            self._name = name
         else:
-            raise ValueError(
-                "Name must be string between 1 and 25 characters.")
+            raise ValueError("Name must be string between 1 and 25 characters.")
 
-    name = property(get_name, set_name)
+    # name = property(get_name, set_name)
 
-    def get_breed(self):
+    # def get_breed(self):
+    #     return self._breed
+
+    @property 
+    def breed(self):
+        """The name property"""
         return self._breed
 
-    def set_breed(self, breed):
+
+    # def set_breed(self, breed):
+    #     if breed in APPROVED_BREEDS:
+    #         self._breed = breed
+    #     else:
+    #         raise ValueError("Breed must be in list of approved breeds.")
+
+    @breed.setter
+    def breed(self, breed):
+        """Breeed must be in the list of approved breed"""
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
             raise ValueError("Breed must be in list of approved breeds.")
 
-    breed = property(get_breed, set_breed)
+    # breed = property(get_breed, set_breed)
